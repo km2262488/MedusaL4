947097f619a654b7634bbc96a9f87c27f8d7bae5
km2262488 <km2262488@gmail.com>
2025-09-17 01:13:24 +0800

    Update MedusaL4.py

--- MedusaL4.py
+++ MedusaL4.py
@@ -45,7 +45,7 @@ def udp(ip, port, packet, times):
         s.close()
     except:
       s.close()
-  print("\x1b[31m[\x1b[33mMEDUSA\x1b[31m] > \x1b[0mTUHAN... JAGA DIA DISAAT KUBERSAMA YG SATUNYA")
+  print("\x1b[31m[\x1b[33mMEDUSA\x1b[31m] > \x1b[0mMo egraste forn, mo egalore fornonte")
 
 def main():
   global threads
